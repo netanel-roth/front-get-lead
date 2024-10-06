33f00ba0dfdb788ab4b644044744c1e10ad0a3c4
DESKTOP-5Q9K2NL\user1 <chay6865@gmail.com>
2024-10-06 10:39:20 +0300

    fix route

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -9,7 +9,7 @@ export const router = createBrowserRouter(
     [
         {
             path: '',
-            element: <Navigate to={paths.TIMER} />
+            element: <Navigate to={paths.LOGIN} />
         },
         {
             path: paths.LOGIN,
