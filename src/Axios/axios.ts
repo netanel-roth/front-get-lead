41a5b4a3b220db963502f1a778f887d71aaf037a
DESKTOP-5Q9K2NL\user1 <chay6865@gmail.com>
2024-10-15 16:07:27 +0300

    Change in axios.ts from port 3000 to 8000

--- src/Axios/axios.ts
+++ src/Axios/axios.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const axiosInstance = axios.create({baseURL:'http://localhost:3000'});
+const axiosInstance = axios.create({baseURL:'http://localhost:8000'});
 
 export default axiosInstance;
