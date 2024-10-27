9c685d25dfbcea62bad6ec86792246bc015b0e99
DESKTOP-5Q9K2NL\user1 <chay6865@gmail.com>
2024-10-27 12:44:49 +0200

    Changing the design of the inputs and changing that only an input and a button will be displayed when clicked

--- src/Components/Login/PasswordInput.tsx
+++ src/Components/Login/PasswordInput.tsx
@@ -1,26 +1,37 @@
-import { Password } from 'primereact/password';
-import { ChangeEvent } from 'react';
-import { messages } from '../../DAL/locales';
-import { ChangeValueProps } from '../../types/loginTypes';
-
+import { InputText } from 'primereact/inputtext';
+import { ChangeEvent, useState } from 'react';
+import { ChangeValueProps } from '../../types/loginTypes'; 
+import 'primeicons/primeicons.css'; 
+import {messages} from '../../DAL/locales'
 
 const PasswordInput = (props: ChangeValueProps) => {
-    const { value, onChangeValue } = props;
+    const { value, onChangeValue } = props; 
+    const [isPasswordVisible, setIsPasswordVisible] = useState(false); 
+
     const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-        event.target.id= 'password'
-        onChangeValue(event)
-    }
+        onChangeValue(event); 
+    };
+
+    const togglePasswordVisibility = () => {
+        setIsPasswordVisible(!isPasswordVisible); 
+    };
 
     return (
-        <Password
-            className='input'
-            value={value}
-            onChange={handleChange}
-            placeholder={messages.login.ENTER_PASSWORD}
-            toggleMask
-            feedback={false}
-        />
-    )
-}
+        <div className="password-input-wrapper">
+            <InputText
+                id="password"
+                type={isPasswordVisible ? 'text' : 'password'}
+                value={value}
+                onChange={handleChange}
+                placeholder={messages.login.ENTER_PASSWORD}
+                className="password-input"
+            />
+            <i
+                className={`password-icon ${isPasswordVisible ? 'pi pi-eye-slash' : 'pi pi-eye'}`}
+                onClick={togglePasswordVisibility}
+            />
+        </div>
+    );
+};
 
 export default PasswordInput;
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -14,29 +14,35 @@ import axios from '../Axios/axios';
 
 const LoginPage: React.FC = () => {
     const navigate = useNavigate();
-    const currentURL = window.location.origin // כדי לא לבצע את הפונקציה הזאת יותר מפעם אחת היא הבסיס לקריאה לתמונות באתר
-    const text = messages.login // במידת הצורך יאפשר שינוי פשוט למקור הטקסט באתר
-    const [userData, setuserData] = useState({ email: '', phone: '', UserName: '', password: '' }); // כדי לשמור את פרטי היוזר
-    const [loginType, setLoginType] = useState<LoginType>('Email'); // מאפשר החלפת מידע על המסך בהתאם לכתוב פה
-    // מתחת לשורה זו יצרתי שני מחסני נתונים קטנים לצורך קריאה פשוטה יותר ושינוי קל יותר במידה ונצטרך להחליף שמות לאייקונים בתיקייה
-    const blueIcons = { call: `${currentURL}/assets/phone-blue.svg`, SMS: `${currentURL}/assets/Icon-awesome-sms.svg`, Email: `${currentURL}/assets/mail-icon-blue.svg` }
-    const whiteIcons = { call: `${currentURL}/assets/phone-icon.svg`, SMS: `${currentURL}/assets/SMS-icon-white.svg`, Email: `${currentURL}/assets/mail-icon.svg` }
-    const [Icons, setIcons] = useState({ ...whiteIcons, Email: blueIcons.Email })
-    const [errorM, seterrorM] = useState('')
-    const dispatch = useAppDispatch();
-    const { isLoading, redirectUrl } = useAppSelector((state) => state.auth); // קבלת ערכים מהסטייט המרכזי
-    
+    const currentURL = window.location.origin;
+    const text = messages.login;
+    const [userData, setuserData] = useState({ email: '', phone: '', password: '' });
+    const [loginType, setLoginType] = useState<LoginType>('Email');
+    const blueIcons = {
+        call: `${currentURL}/assets/phone-blue.svg`,
+        SMS: `${currentURL}/assets/Icon-awesome-sms.svg`,
+        Email: `${currentURL}/assets/mail-icon-blue.svg`
+    };
+    const whiteIcons = {
+        call: `${currentURL}/assets/phone-icon.svg`,
+        SMS: `${currentURL}/assets/SMS-icon-white.svg`,
+        Email: `${currentURL}/assets/mail-icon.svg`
+    };
+    const [Icons, setIcons] = useState({ ...whiteIcons, Email: blueIcons.Email });
+    const [errorM, seterrorM] = useState('');
     const [showPasswordInput, setShowPasswordInput] = useState(false);
+    const dispatch = useAppDispatch();
+    const { isLoading, redirectUrl } = useAppSelector((state) => state.auth);
+
     const setUser = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const user: any = { ...userData }
-        user[e.target.id] = e.target.value
-        setuserData(user)
-    }
+        const user = { ...userData };
+        user[e.target.id] = e.target.value;
+        setuserData(user);
+    };
 
     const handleSendPassword = async () => {
         if (userData.email || userData.phone) {
             try {
-                //קוד שפונה לאוריגמי וגורם לשליחת קוד אימות למשתמש
                 setShowPasswordInput(true);
                 seterrorM('');
             } catch (error) {
@@ -50,30 +56,54 @@ const LoginPage: React.FC = () => {
     const handleLogin = async () => {
         if ((userData.email || userData.phone) && userData.password) {
             const identifier = loginType === 'Email' ? userData.email : userData.phone;
-            
+
             try {
                 const resultAction = await dispatch(
                     loginUser({
-                        identifier: identifier,
+                        identifier,
                         identifierType: loginType,
                         password: userData.password
                     })
                 );
-    
+
                 if (loginUser.fulfilled.match(resultAction)) {
-                    const token = resultAction.payload.data.token; 
-                    localStorage.setItem('token', token); 
-                    seterrorM(''); 
-                    navigate('/details'); 
+                    const token = resultAction.payload.data.token;
+                    localStorage.setItem('token', token);
+                    seterrorM('');
+                    navigate('/details');
                 } else {
                     console.log('Login failed:', resultAction.payload);
-                    seterrorM(messages.Error.WRONG_INPUT); 
+                    seterrorM(messages.Error.WRONG_INPUT);
                 }
             } catch (error) {
                 console.log('An error occurred:', error);
             }
         } else {
-            seterrorM(messages.Error.WRONG_INPUT); 
+            seterrorM(messages.Error.WRONG_INPUT);
+        }
+    };
+
+    const clickType = (type: LoginType) => {
+        setLoginType(type);
+        const icons = { ...whiteIcons };
+        icons[type] = blueIcons[type];
+        setIcons(icons);
+    };
+
+    const offFocusType = (type: LoginType) => {
+        if (loginType !== type) {
+            const icons = { ...Icons };
+            icons[type] = whiteIcons[type];
+            setIcons(icons);
+        }
+    };
+
+    const hover = (type: LoginType) => {
+        if (loginType !== type) {
+            const icons = { ...whiteIcons };
+            icons[type] = blueIcons[type];
+            icons[loginType] = blueIcons[loginType];
+            setIcons(icons);
         }
     };
 
@@ -81,64 +111,69 @@ const LoginPage: React.FC = () => {
         window.location.href = redirectUrl;
     }
 
-    const clickType = (type: LoginType) => {
-        setLoginType(type)
-        const icons = { ...whiteIcons }
-        icons[type] = blueIcons[type]
-        setIcons(icons)
-    }
-
-    const offFocusType = (type: LoginType) => {
-        if (loginType !== type) {
-            const icons = { ...Icons }
-            icons[type] = whiteIcons[type]
-            setIcons(icons)
-        }
-    }
-
-    const hover = (type: LoginType) => {
-        if (loginType !== type) {
-            const icons = { ...whiteIcons }
-            icons[type] = blueIcons[type]
-            icons[loginType] = blueIcons[loginType]
-            setIcons(icons)
-        }
-    }
-
     return (
-        <div className='container'>
-            <div>
-                <label>{text.CHOOSE_LOGIN_TYPE}</label>
-                <p className='red'>{errorM}</p>
-                <div className='loginTypes'>
-                    <button id='call' className={`${loginType === 'call' ? 'choosen' : 'notChoosenB'} button-with-image`} onClick={() => clickType('call')} onMouseOver={() => hover('call')} onMouseOut={() => offFocusType('call')}>
-                        <img src={Icons.call} alt='לוגו'></img>
-                        <span>{text.PHONE}</span>
-                    </button>
-                    <button className={`${loginType === 'Email' ? 'choosen' : 'notChoosenB'} button-with-image`} onClick={() => clickType('Email')} onMouseOver={() => hover('Email')} onMouseOut={() => offFocusType('Email')}>
-                        <img src={Icons.Email} alt='לוגו'></img>
-                        <span>{text.TO_MAIL}</span>
-                    </button>
-                    <button className={`${loginType === 'SMS' ? 'choosen' : 'notChoosenB'} button-with-image`} onClick={() => clickType('SMS')} onMouseOver={() => hover('SMS')} onMouseOut={() => offFocusType('SMS')}>
-                        <img src={Icons.SMS} alt='לוגו'></img>
-                        <span>{text.TO_SMS}</span>
-                    </button>
-                </div>
-            </div >
-            {loginType === 'Email' ?
-                <EmailInput value={userData.email} onChangeValue={setUser} /> :
-                <PhoneInput value={userData.phone} onChangeValue={setUser} />
-            }
 
-            {!showPasswordInput ? (
-                <ContinueButton onLogin={handleSendPassword} />
-            ) : (
-                <>
-                    <PasswordInput value={userData.password} onChangeValue={setUser} />
-                    <ContinueButton onLogin={handleLogin} />
-                </>
+        <div className="container">
+            {!showPasswordInput && (
+                <div>
+                    <label>{text.CHOOSE_LOGIN_TYPE}</label>
+                    <p className="red">{errorM}</p>
+                    <div className="loginTypes">
+                        <button
+                            id="call"
+                            className={`${loginType === 'call' ? 'choosen' : 'notChoosenB'} button-with-image`}
+                            onClick={() => clickType('call')}
+                            onMouseOver={() => hover('call')}
+                            onMouseOut={() => offFocusType('call')}
+                        >
+                            <img src={Icons.call} alt="לוגו" />
+                            <span>{text.PHONE}</span>
+                        </button>
+                        <button
+                            className={`${loginType === 'Email' ? 'choosen' : 'notChoosenB'} button-with-image`}
+                            onClick={() => clickType('Email')}
+                            onMouseOver={() => hover('Email')}
+                            onMouseOut={() => offFocusType('Email')}
+                        >
+                            <img src={Icons.Email} alt="לוגו" />
+                            <span>{text.TO_MAIL}</span>
+                        </button>
+                        <button
+                            className={`${loginType === 'SMS' ? 'choosen' : 'notChoosenB'} button-with-image`}
+                            onClick={() => clickType('SMS')}
+                            onMouseOver={() => hover('SMS')}
+                            onMouseOut={() => offFocusType('SMS')}
+                        >
+                            <img src={Icons.SMS} alt="לוגו" />
+                            <span>{text.TO_SMS}</span>
+                        </button>
+                    </div>
+                </div>
             )}
+
+            <div className="input-button-wrapper">
+                {!showPasswordInput ? (
+                    loginType === 'Email' ? (
+                        <div className="input-container">
+                            <EmailInput value={userData.email} onChangeValue={setUser} />
+                        </div>
+                    ) : (
+                        <div className="input-container">
+                            <PhoneInput value={userData.phone} onChangeValue={setUser} />
+                        </div>
+                    )
+                ) : (
+                    <div className="input-container">
+                        <PasswordInput value={userData.password} onChangeValue={setUser} />
+                    </div>
+                )}
+
+                <ContinueButton
+                    onLogin={!showPasswordInput ? handleSendPassword : handleLogin}
+                />
+            </div>
         </div>
+
     );
 };
 
